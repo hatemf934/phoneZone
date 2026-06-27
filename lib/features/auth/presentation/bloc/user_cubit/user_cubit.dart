4c4639afcbe09b9c0d0d169553665160753f8fcb
hatemfathy <hatemf934@gmail.com>
2026-06-28 01:28:46 +0300

    Refactor UserCubit to include userRepoImlpement dependency; initialize form keys and text controllers

--- lib/features/auth/presentation/bloc/user_cubit/user_cubit.dart
+++ lib/features/auth/presentation/bloc/user_cubit/user_cubit.dart
@@ -4,20 +4,48 @@ import 'package:meta/meta.dart';
 import 'package:phone_zone/core/error/failure.dart';
 import 'package:phone_zone/features/auth/data/model/sign_in_model.dart';
 import 'package:phone_zone/features/auth/data/model/sign_up_model.dart';
+import 'package:phone_zone/features/auth/data/repos/user_repo_imlpement.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  UserCubit(this.userRepoImlpement) : super(UserInitial());
 
-  GlobalKey? formSignInKey;
-  GlobalKey? formSignUpKey;
+  final UserRepoImlpement userRepoImlpement;
+  GlobalKey<FormState> formSignInKey = GlobalKey();
+  GlobalKey<FormState> formSignUpKey = GlobalKey();
 
-  TextEditingController? usernameSignIn;
-  TextEditingController? passwordSignIn;
+  TextEditingController usernameSignIn = TextEditingController();
+  TextEditingController passwordSignIn = TextEditingController();
 
-  TextEditingController? emailSignUp;
-  TextEditingController? passwordSignUp;
-  TextEditingController? usernameSignUp;
-  TextEditingController? phoneSignUp;
+  TextEditingController emailSignUp = TextEditingController();
+  TextEditingController passwordSignUp = TextEditingController();
+  TextEditingController usernameSignUp = TextEditingController();
+  TextEditingController phoneSignUp = TextEditingController();
+
+  Future<void> signUp() async {
+    emit(SignUpLoading());
+    final result = await userRepoImlpement.signUp(
+      email: emailSignUp.text,
+      password: passwordSignUp.text,
+      phone: passwordSignUp.text,
+      username: usernameSignUp.text,
+    );
+    result.fold(
+      (failure) => emit(SignUpFailure(failure: failure)),
+      (signUpModel) => emit(SignUpSuccess(signUpModel: signUpModel)),
+    );
+  }
+
+  Future<void> signIn() async {
+    emit(SignInLoading());
+    final result = await userRepoImlpement.signIn(
+      password: passwordSignIn.text,
+      username: usernameSignIn.text,
+    );
+    result.fold(
+      (failure) => emit(SignInFaiure(failure: failure)),
+      (signInModel) => emit(SignInSuccess(signInModel: signInModel)),
+    );
+  }
 }
